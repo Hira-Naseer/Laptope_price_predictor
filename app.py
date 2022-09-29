cc979c71119b2fd48e97edac8ccb0066bd7e7474
= <201980036@gift.edu.pk>
2022-09-29 17:31:31 +0500

    changings in app file

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ ram = st.selectbox('RAM(in GB',[2,4,6,8,12,16,24,32,64])
 weight = st.number_input('Weight of the laptope')
 touchscreen = st.selectbox('Touchscreen',['No','Yes'])
 ips = st.selectbox('IPS',['No','Yes'])
-screen_size = st.number_input('Screen Size')
+screen_size = st.number_input('Screen Size', min_value=1.0)
 
 resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2560x1600','2560x1440','2304x1440'])
 
 cpu = st.selectbox('CPU',df['Cpu brand'].unique())
-hdd = st.selectbox('HDD(in GB',[0,128,256,512,1024,2048])
-ssd = st.selectbox('SSD(in GB',[0,8,128,256,512,1024])
+hdd = st.selectbox('HDD(in GB',[128,256,512,1024,2048])
+ssd = st.selectbox('SSD(in GB',[8,128,256,512,1024])
 gpu = st.selectbox('GPU',df['Gpu brand'].unique())
 os = st.selectbox('OS',df['os'].unique())
 if st.button('Predict Price'):
@@ -38,6 +38,7 @@ if st.button('Predict Price'):
 
     X_res = int(resolution.split('x')[0])
     Y_res = int(resolution.split('x')[1])
+
     ppi = ((X_res**2)+(Y_res**2))**0.5/screen_size
 
     query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
